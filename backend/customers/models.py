9aac97279792e31c3d45a74c36e664dab1ea793c
saman <saman.sahebi@gmail.com>
2023-07-18 10:46:34 +0330

    backend init

---
+++ backend/customers/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+
+class Customers(models.Model):
+    pass
---
+++ backend/shop/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Provider(models.Model):
+    pass
+
+
+class Products(models.Model):
+    pass
