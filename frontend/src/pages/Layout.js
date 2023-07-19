6837579c432032b9cbf3164075a0d23a8387c010
saman <saman.sahebi@gmail.com>
2023-07-19 17:39:43 +0330

    slider added

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -1,72 +1,95 @@
-import { HiMenu } from "react-icons/hi";
+// import { HiMenu } from "react-icons/hi";
 import { HiHome } from "react-icons/hi";
 import { FaShop } from "react-icons/fa6";
-import slide from '../assets/uUz0dI-black-coffee-cup-hd-image.png'
+import { FaBagShopping } from "react-icons/fa6";
+// import { FaUserLarge } from "react-icons/fa6";
+import { FaNewspaper } from "react-icons/fa6";
+import slide from '../assets/mike-kenneally-TD4DBagg2wE-unsplash.jpg'
+import slide2 from '../assets/nathan-dumlao-Y3AqmbmtLQI-unsplash.jpg'
+import slide3 from '../assets/nathan-dumlao-pMW4jzELQCw-unsplash.jpg'
 import { BottomNavigation } from 'reactjs-bottom-navigation'
 import 'reactjs-bottom-navigation'
 // import Home from './Home'
 // import Shop from './Shop'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 
 
 export default function Layout() {
       
         const bottomNavItems = [
-          {
-            title: 'Home',
       
+          {
+            title: 'وبلاگ',
+      
+            icon: <FaNewspaper size={25} color={'black'}/>,
+      
+            activeIcon: <FaNewspaper size={25} color={'red'}/>,
+      
+            // onClick: () => alert('menu clicked')
+          },
+          {
+            title: 'سبد خرید',
+
+            icon: <FaBagShopping size={25} color={'black'}/>,
+
+            activeIcon: <FaBagShopping size={25} color={'red'}/>
+          },
+
+          {
+            title: 'فروشگاه',
+
+            icon: <FaShop size={25} color={'black'}/>,
+
+            activeIcon: <FaShop size={25} color={'red'}/>
+          },
+
+          {
+            title: 'خانه',
+
             icon: <HiHome size={25} color={'black'}/>,
-      
-            activeIcon: <HiHome size={25} color={'white'}/>
+
+            activeIcon: <HiHome size={25} color={'red'}/>
           },
-      
-          {
-            title: 'Search',
-      
-            icon: <HiHome size={25} color={'white'}/>,
-      
-            activeIcon: <HiHome size={25} color={'white'}/>
-          },
-      
-          {
-            title: 'Notifications',
-      
-            icon: <HiHome size={25} color={'white'}/>,
-      
-            activeIcon: <HiHome size={25} color={'white'}/>
-          },
-      
-          {
-            title: 'Menu',
-      
-            icon: <HiHome size={25} color={'white'}/>,
-      
-            activeIcon: <HiHome size={25} color={'white'}/>,
-      
-            onClick: () => alert('menu clicked')
-          }
         ]
 
     return (
-        <div className='fixed h-screen w-screen bg-gray-900'>
+        <div className='fixed h-screen w-screen bg-gray-100'>
             <div className='flex justify-between w-screen px-4 py-4'>
-                <div className=''>
-                    <HiMenu size={25} color={'white'}/>
-                </div>
-                <div className='flex flex-row-reverse text-white'>
-                    <div className={'pl-4'}>
-                        <HiHome size={25} color={'white'}/>
-                    </div>
-                    <div className={'pl-4'}>
-                        <FaShop size={25} color={'white'}/>
-                    </div>
-              </div>
+                <input className='bg-gray-200 p-2 rounded w-full text-right shadow-md' placeholder='جستجوی محصولات'/>
             </div>
-            {/* <div className='flex flex-col h-full items-center'> */}
+            <div className='flex w-full h-full px-4'>
+                    <Swiper
+                      spaceBetween={50}
+                      slidesPerView={1}
+                      onSlideChange={() => console.log('slide change')}
+                      onSwiper={(swiper) => console.log(swiper)}
+                    >
+                         <SwiperSlide>
+                              <div className='flex bg-gray-100 w-full h-48 rounded'>
+
+                                <img alt={'slide'} src={slide} className='object-contain'/>
+                              </div>
+                         </SwiperSlide>
+                         <SwiperSlide>
+                             <div className='flex bg-gray-100 w-full h-48 rounded'>
+                                <img alt={'slide'} src={slide2} className='object-contain'/>
+                            </div>
+                         </SwiperSlide>
+                         <SwiperSlide>
+                             <div className='flex bg-gray-100 w-full h-48 rounded'>
+                                <img alt={'slide'} src={slide3} className='object-contain'/>
+                            </div>
+                         </SwiperSlide>
+
+                    </Swiper>
+            </div>
+             <div className='flex flex-col h-full items-center'>
+
                 <div className='text-white p-4'>
                     فروش انواع قهوه و سیروپ و دستگاه اسپرسو ساز
                 </div>
-                <img alt={'slide'} src={slide} className='py-6' style={{ width: "75%", height: "25%" }}/>
                 <div className="rounded p-2">
                 <BottomNavigation
                     items={bottomNavItems}
@@ -74,6 +97,7 @@ export default function Layout() {
                     onItemClick={(item) => console.log(item)}
                 />
                 </div>
+                 </div>
         </div>
     )
 }
