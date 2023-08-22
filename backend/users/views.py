531cc947c89d77c1586a89f7ca0c1c2c1d406b74
saman <saman.sahebi@gmail.com>
2023-08-22 12:29:27 +0330

    login backend bug fix

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,18 +1,14 @@
-import time
-from datetime import datetime, timedelta
+from datetime import timedelta
 from random import randint
 from rest_framework import status, exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import OTP, User
+from django.utils import timezone
 from django.conf import settings
 from kavenegar import *
 from django.utils.translation import gettext_lazy as _
-import datetime
-import pytz
-
-utc = pytz.UTC
 
 
 class Login(APIView):
@@ -30,19 +26,19 @@ class Login(APIView):
         data = request.data.copy()
 
         phone_number = data.get('phone_number')
-        otp_code = data.get('otp')
-        current_time = datetime.now()
+        otp_code = int(data.get('otp'))
+        current_time = timezone.now()
 
         try:
             query = OTP.objects.get(user__phone_number=phone_number)
         except:
             raise exceptions.NotFound('otp code is not correct')
 
-        generated_otp = query.otp_code
+        generated_otp = int(query.otp_code)
         time_created = query.created_at
 
         if generated_otp == otp_code:
-            if time_created.replace(tzinfo=None) + datetime.min(5) < current_time:
+            if time_created + timedelta(minutes=5) > current_time:
                 user = User.objects.get(phone_number=phone_number)
                 user.active = True
                 user.save()
@@ -67,13 +63,15 @@ class SendOTP(APIView):
         otp_model = OTP.objects.filter(user__phone_number=phone_number)
         if otp_model.exists():
             time_created = otp_model.first().created_at
-            current_time = datetime.now()
-            if time_created.replace(uzinfo=None) + timedelta(minutes=5) < current_time:  # 5 Minutes
+            current_time = timezone.now()
+            if time_created + timedelta(minutes=5) < current_time:  # 5 Minutes
                 self.otp_create_send(user)
+                return Response({'detail': 'otp code has been sent to your mobile number'})
             else:
-                return Response({'please wait 5 minutes and try again'})
+                return Response({'please wait 5 minutes and try again'}, status=status.HTTP_403_FORBIDDEN)
         else:
             self.otp_create_send(user)
+            return Response({'detail': 'otp code has been sent to your mobile number'})
 
     @staticmethod
     def otp_create_send(user):
@@ -82,12 +80,13 @@ class SendOTP(APIView):
         while OTP.objects.filter(otp_code=otp).exists():
             otp = randint(100000, 999999)
         otp_model = OTP.objects.create(otp_code=otp, user=user)
-        # api = KavenegarAPI(settings.KAVENEGAR_API_KEY)
-        # params = {
-        #     'sender': '10004346',
-        #     'receptor': user.phone_number,
-        #     'message': _(f'your otp code is: {otp_model.otp_code}')
-        # }
-        # response = api.sms_send(params)
+        api = KavenegarAPI(settings.KAVENEGAR_API_KEY)
+        params = {
+            'sender': '10004346',
+            'receptor': user.phone_number,
+            'message': _(f'your otp code is: {otp_model.otp_code}')
+        }
+        response = api.sms_send(params)
+        print(response)
         print(otp_model.otp_code)
         return None
